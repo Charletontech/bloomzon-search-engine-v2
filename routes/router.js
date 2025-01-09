a2856bfdfcd1342b07df18f0e3522cb9e2ace21d
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2025-01-10 00:11:40 +0100

    prettier model

--- routes/router.js
+++ routes/router.js
@@ -13,7 +13,7 @@ const {
 router.get("/", viewController);
 
 // private routes
-router.post("/api/image-search", imageSearchControllerV2);
+router.post("/api/image-search", imageSearchController);
 router.post("/api/barcode-search", barcodeSearchController);
 router.post("/api/voice-search", voiceSearchController);
 module.exports = router;
